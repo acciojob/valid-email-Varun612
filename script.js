70b7426df54b88312dc15be97e18353ae43b37d9
Varun Kandhari <75046190+Varun612@users.noreply.github.com>
2025-05-12 07:56:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function validEmail(str) {
-  //your JS code here.
+  if(typeof str !== 'string') return false
+  str=str.trim()
+  if(str === '') return false
+  const emailRegex = /^[A-Za-z0-9]+(?:[._-][A-Za-z0-9]+)*@[A-Za-z0-9]+(?:[._-][A-Za-z0-9]+)*(?:\.[A-Za-z]{2,})+$/
+  return emailRegex.test(str)
 }
 
 // Do not change the code below.
